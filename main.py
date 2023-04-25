d784be11976716302380b6b8e260bbb351b42bfc
Erik Papernyuk <80971885+Erp4759@users.noreply.github.com>
2023-04-25 18:05:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import openai
+
+
+def generate_lyrics(mess):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {'role': 'user', 'content': mess}
+        ],
+        temperature=0
+    )
+    return response['choices'][0]['message']['content']
+
+
+st.title("SoundScript")
+openai.api_key = st.text_input("Enter your open OpenAI API:")
+
+prompt = st.text_input("What will the song be about?")
+mood = st.selectbox("Select the mood of the song", ["Happy", "Sad", "Romantic", "Energetic"])
+character = st.text_input("Enter additional details of the song")
+
+res = f"Write the song about {prompt}, with {mood} mood."
+if len(character) > 2:
+    res += f"Details of the song: {character}"
+if st.button("Generate Lyrics"):
+    song = generate_lyrics(res)
+    st.write("Text:")
+    st.write(prompt)
+    st.text(song)
+    st.write("Chords:")
+    chords = generate_lyrics("Generate guitar chords for this text: \n" + prompt)
+    st.text(chords)
+    print(song)
+    print(chords)
